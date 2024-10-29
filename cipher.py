2d02b3190afa6162326652bc648b5374c540a39d
Camilla Babb <camilla.babb@gmail.com>
2024-10-28 23:08:39 -0400

    added code to cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,40 @@
-# add your code here
+cypher = {
+    "a": "f",
+    "b": "g",
+    "c": "h",
+    "d": "i",
+    "e": "j",
+    "f": "k",
+    "g": "l",
+    "h": "m",
+    "i": "n",
+    "j": "o",
+    "k": "p",
+    "l": "q",
+    "m": "r",
+    "n": "s",
+    "o": "t",
+    "p": "u",
+    "q": "v",
+    "r": "w",
+    "s": "x",
+    "t": "y",
+    "u": "z",
+    "v": "a",
+    "w": "b",
+    "x": "c",
+    "y": "d",
+    "z": "e",
+}
+
+initial_sentence = input("Please enter a sentence to be encrypted: ").lower()
+
+
+cypher_sentence = ""
+
+for char in initial_sentence:
+    if char in cypher:
+      char = cypher[char]
+    cypher_sentence += char
+
+print(cypher_sentence)
